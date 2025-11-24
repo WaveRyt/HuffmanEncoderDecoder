12d8d68e126d1f0b63801a8cddf2dabffec372aa
WaveRyt <sritesh8621@gmail.com>
2025-11-24 11:43:16 +0530

    Update node.py

--- src/node.py
+++ src/node.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 @dataclass(order= True)
 class Node:
     freq: int 
-    symbol: Optional[bytes] = None
-    left: Optional[Node] = None
-    right: Optional[Node] = None
+    symbol: Optional[bytes] = field(default = None, compare= False)
+    left: Optional[Node] = field(default = None, compare= False)
+    right: Optional[Node] = field(default = None, compare= False)
 
     def is_leaf(self) -> bool:
         return self.left is None and self.right is None
